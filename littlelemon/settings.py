b332f9e583c76c9b3ce2a91bbbcbcf666519c5bd
samioco <samio1221@gmail.com>
2023-10-03 16:18:26 -0400

    mysql etc

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -74,13 +74,44 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'littlelemon',  
+        'USER': 'root',  
+        'PASSWORD': 'samisthemanbciam',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
+# DATABASES = {
+#     'default': { 
+#         'ENGINE': 'django.db.backends.sqlite3', 
+#         'NAME': BASE_DIR / 'db.sqlite3', 
+#     },
+#     'mydb': {   
+ 
+#         'ENGINE': 'django.db.backends.mysql',   
+#         'NAME': 'my_database',   
+#         'USER': 'root',   
+#         'PASSWORD': 'samisthemanbciam',   
+#         'HOST': '127.0.0.1',   
+#         'PORT': '3306',   
+#         'OPTIONS': {   
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
